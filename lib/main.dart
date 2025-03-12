25f9aeb2baffde01039a3b101cc8304cb288c955
FRIDA_ABRIL_CISNEROS_HERNANDEZ <a.22308051281162@cbtis128.edu.mx>
2025-03-12 19:46:05 +0000

    examen

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,114 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+class Cliente {
+  final int idCliente;
+  final String nombre;
+  final String direccion;
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+  Cliente({
+    required this.idCliente,
+    required this.nombre,
+    required this.direccion,
+  });
+}
+
+class ClienteContainer extends StatelessWidget {
+  final Cliente cliente;
+  final Color color; // Agregamos un parámetro para el color
+
+  ClienteContainer({required this.cliente, required this.color});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
+    return Container(
+      padding: EdgeInsets.all(16.0),
+      margin: EdgeInsets.all(8.0),
+      decoration: BoxDecoration(
+        color: color, // Usamos el color proporcionado
+
+        borderRadius: BorderRadius.circular(8.0),
+        boxShadow: [
+          // Agregamos la sombra
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.5),
+            spreadRadius: 5,
+            blurRadius: 7,
+            offset: Offset(0, 3), // changes position of shadow
+          ),
+        ],
       ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text('ID: ${cliente.idCliente}'),
+          Text('Nombre: ${cliente.nombre}'),
+          Text('Dirección: ${cliente.direccion}'),
+        ],
+      ),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
+void main() {
+  runApp(MyApp());
+}
 
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Clientes'),
+          centerTitle: true,
+          backgroundColor: Color(0xff60ccd4), // Agrega color a la AppBar
+        ),
+        body: ListView(
+          children: [
+            ClienteContainer(
+              cliente: Cliente(
+                idCliente: 22308051281162,
+                nombre: 'Frida Abril Cisneros Hernandez',
+                direccion: 'Ciudad',
+              ),
+              color: Color(0xffb3c4fc)!, // Color para el primer contenedor
+            ),
+            ClienteContainer(
+              cliente: Cliente(
+                idCliente: 2,
+                nombre: 'María García',
+                direccion: 'Avenida 456, Pueblo',
+              ),
+              color: Color(0xff98a2fd)!, // Color para el segundo contenedor
+            ),
+            ClienteContainer(
+              cliente: Cliente(
+                idCliente: 3,
+                nombre: 'Pedro López',
+                direccion: 'Plaza Mayor, Villa',
+              ),
+              color: Color(0xffb3c4fc)!, // Color para el tercer contenedor
+            ),
+            ClienteContainer(
+              // Agrega un cuarto contenedor
+              cliente: Cliente(
+                idCliente: 4,
+                nombre: 'Laura Martinez',
+                direccion: 'Calle Sol, Pueblo',
+              ),
+              color: Color(0xff9ba8ef)!,
+            ),
+            ClienteContainer(
+              // Agrega un quinto contenedor
+              cliente: Cliente(
+                idCliente: 5,
+                nombre: 'Carlos Ramirez',
+                direccion: 'Avenida Luna, Ciudad',
+              ),
+              color: Color(0xffb3c4fc)!,
+            ),
+          ],
         ),
       ),
     );
